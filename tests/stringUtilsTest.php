4ddd65d85d7f83c8da30bf9afe96ad68c302a964
igor.pvdc <igor.pvdc@gmail.com>
2021-11-25 19:00:59 +0000

    added Webmozart\Assert extetion for autotesting

--- tests/stringUtilsTest.php
+++ tests/stringUtilsTest.php
@@ -1,13 +1,18 @@
 <?php
 
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
 use function StringUtils\capitalize;
+use Webmozart\Assert\Assert;
 
-if (capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize(''), '');
 
 echo 'Все тесты пройдены!';
