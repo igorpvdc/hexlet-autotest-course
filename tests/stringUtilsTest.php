a50ff228bf91f6ed0b2a39148c0528c0834bd7ba
igor.pvdc <igor.pvdc@gmail.com>
2021-11-25 17:17:14 +0000

    added stringUtils.php and test for it

---
+++ tests/stringUtilsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use function StringUtils\capitalize;
+
+if (capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
